c543e2f23f18730b92fdde346c79843568931d34
MohonSaha <practicewithmohon@gmail.com>
2024-05-10 21:07:34 +0600

    hero section part-2

--- src/components/UI/HomePage/HeroSection/HeroSection.tsx
+++ src/components/UI/HomePage/HeroSection/HeroSection.tsx
@@ -48,7 +48,6 @@ const HeroSection = () => {
           component="p"
           fontWeight={400}
           sx={{
-            width: "50%",
             my: 4,
           }}
           color="secondary.main"
@@ -68,7 +67,81 @@ const HeroSection = () => {
           <Button variant="outlined">Contact Us</Button>
         </Box>
       </Box>
-      <Box>Right</Box>
+      <Box
+        sx={{
+          p: 1,
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          position: "relative",
+          mt: 0,
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            left: "200px",
+            top: "-30px",
+          }}
+        >
+          <Image src={assets.svgs.arrow} width={100} height={100} alt="arrow" />
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Box mt={4}>
+            <Image
+              src={assets.images.doctor1}
+              alt="doctor1"
+              height={380}
+              width={240}
+            />
+          </Box>
+          <Box>
+            <Image
+              src={assets.images.doctor2}
+              alt="doctor2"
+              height={350}
+              width={240}
+            />
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            position: "absolute",
+            top: "220px",
+            left: "150px",
+          }}
+        >
+          <Image
+            src={assets.images.doctor3}
+            alt="doctor3"
+            width={240}
+            height={240}
+          />
+        </Box>
+
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "-50px",
+            right: 0,
+            zIndex: "-1",
+          }}
+        >
+          <Image
+            src={assets.images.stethoscope}
+            alt="doctor3"
+            width={180}
+            height={180}
+          />
+        </Box>
+      </Box>
     </Container>
   );
 };
